d2abcf615f717282b2ebca38fa6df1a9dad4df85
Olaf Roeder <github@olaf-roeder.de>
2023-02-14 20:40:37 +0100

    moved demo from src/main to src/test

---
+++ src/test/java/demo/application/DemoRunner.java
@@ -0,0 +1,9 @@
+package demo.application;
+
+import javafx.application.Application;
+
+public class DemoRunner {
+    public static void main(String[] args) {
+        Application.launch(Main.class);
+    }
+}
