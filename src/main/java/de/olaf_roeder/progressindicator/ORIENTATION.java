ca8fc26f6e39ab888026221f70020fb6d4e9e593
Olaf Roeder <github@olaf-roeder.de>
2024-11-02 13:32:42 +0100

    added JavaDoc for enum ORIENTATION;

--- src/main/java/de/olaf_roeder/progressindicator/ORIENTATION.java
+++ src/main/java/de/olaf_roeder/progressindicator/ORIENTATION.java
@@ -1,6 +1,15 @@
 package de.olaf_roeder.progressindicator;
 
+/**
+ * Orientation of the {@link ImageProgressIndicator}
+ */
 public enum ORIENTATION {
+    /**
+     * HORIZONTAL for a horizontal progress bar
+     */
     HORIZONTAL,
+    /**
+     * VERTICAL for a vertical progress bar (also suggested for square images)
+     */
     VERTICAL
 }
